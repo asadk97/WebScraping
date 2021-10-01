9b0118436caf75da805d6b127e459119cdbe40b0
asadk97 <32286739+asadk97@users.noreply.github.com>
2021-10-01 12:14:39 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
+import requests
 from flask import (Flask, render_template, request,)
 # from model import db, save_db
-import main_task
+from main import main_task
 
 app = Flask(__name__)
 
@@ -10,11 +11,23 @@ def homepage():
     if request.method == "GET":
         return render_template(
             "index.html"
-    )
+        )
+
     else:
         url = request.form.get("user_input")
-        soup = main_task.get_content(url)
-        return render_template("results.html", output=soup)
+        try:
+            requests.get(url)
+        except:
+            soup = "Invalid URL please enter a valid URL"
+            return render_template("results.html", output=soup)
+
+        else:
+            if requests.get(url).status_code == 200:
+                soup = main_task.get_content(url)
+                return render_template("results.html", output=soup)
+            else:
+                output = "Invalid URL please enter a valid URL"
+                return render_template("results.html", output=output)
 
 
 @app.route("/results")
